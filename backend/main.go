85d766b0f58964b7d871d63341b01a41fae1cc05
Grantsuu <tnarg122@gmail.com>
2023-09-27 03:20:51 +0000

    Made types file and display user token

--- backend/main.go
+++ backend/main.go
@@ -1,56 +1,77 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"grant-tech-yodlee.com/types"
 )
 
-type GetUserToken struct {
-	ClientId string `json:"clientId"`
-	Secret   string `json:"secret"`
-}
-
-type AccountResponse struct {
-	Name string `json:"name"`
-}
+const (
+	YodleeAuthTokenUrl   = "https://sandbox.api.yodlee.com/ysl/auth/token"
+	YodleeAuthAPIVersion = "1.1"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/accountInfo", getAccountInfo)
+	router.GET("/api/accountInfo", getAccountInfo)
 
 	router.Run("localhost:8080")
 }
 
 func getAccountInfo(c *gin.Context) {
-	getAccessToken()
-	c.IndentedJSON(http.StatusOK, AccountResponse{Name: "Grant"})
+	res, _ := getUserToken()
+	c.IndentedJSON(http.StatusOK, res)
 
 }
 
-func getAccessToken() {
+func getUserToken() (interface{}, error) {
 	data := url.Values{}
 	data.Add("clientId", getEnvVariable("CLIENT_ID"))
 	data.Add("secret", getEnvVariable("SECRET"))
 
-	req, _ := http.NewRequest(http.MethodPost, "https://sandbox.api.yodlee.com/ysl/auth/token", strings.NewReader(data.Encode()))
-	req.Header.Set("Api-Version", "1.1")
+	req, err := http.NewRequest(http.MethodPost, YodleeAuthTokenUrl, strings.NewReader(data.Encode()))
+	if err != nil {
+		log.Printf("error creating new request: %v\n", err)
+		return nil, err
+	}
+
+	req.Header.Set("Api-Version", YodleeAuthAPIVersion)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("loginName", getEnvVariable("USER_NAME"))
 
 	client := &http.Client{}
 
-	res, _ := client.Do(req)
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("error sending http request: %v\n", err)
+		return nil, err
+	}
 
-	responseBody, _ := io.ReadAll(res.Body)
+	responseBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("error reading http response: %v\n", err)
+		return nil, err
+	}
 	res.Body.Close()
 
-	fmt.Println(string(responseBody))
+	userToken := types.UserToken{}
+
+	err = json.Unmarshal(responseBytes, &userToken)
+	if err != nil {
+		log.Printf("error unmarshalling response bytes: %v\n", err)
+		return nil, err
+	}
+
+	log.Printf("user token received: %v\n", userToken)
+	return userToken, nil
 }
 
 func getEnvVariable(key string) string {
