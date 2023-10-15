ff9fd0ede5e9711bdf87c2b446dd6ca2d866f3f6
Grantsuu <tnarg122@gmail.com>
2023-10-15 20:23:07 +0000

    First pass at adding a get account info endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"log"
@@ -13,9 +14,14 @@ import (
 	"grant-tech-yodlee.com/types"
 )
 
+type key int
+
 const (
-	YodleeAuthTokenUrl   = "https://sandbox.api.yodlee.com/ysl/auth/token"
-	YodleeAuthAPIVersion = "1.1"
+	YodleeApiHostUrl               = "https://sandbox.api.yodlee.com/ysl/"
+	YodleeAuthTokenPath            = "auth/token"
+	YodleeVerifiedAccountsPath     = "verification/verifiedAccounts"
+	YodleeAuthApiVersion           = "1.1"
+	accessToken                key = iota
 )
 
 func main() {
@@ -23,6 +29,7 @@ func main() {
 	router.Use(cors.Default())
 
 	router.POST("/api/userToken", postUserToken)
+	router.GET("/api/accounts", getAccountInformation)
 
 	router.Run(":8080")
 }
@@ -40,13 +47,13 @@ func postUserToken(c *gin.Context) {
 	data.Add("clientId", body.ClientId)
 	data.Add("secret", body.Secret)
 
-	req, err := http.NewRequest(http.MethodPost, YodleeAuthTokenUrl, strings.NewReader(data.Encode()))
+	req, err := http.NewRequest(http.MethodPost, YodleeApiHostUrl+YodleeAuthTokenPath, strings.NewReader(data.Encode()))
 	if err != nil {
 		log.Printf("error creating new request: %v\n", err)
 		return
 	}
 
-	req.Header.Set("Api-Version", YodleeAuthAPIVersion)
+	req.Header.Set("Api-Version", YodleeAuthApiVersion)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("loginName", body.UserName)
 
@@ -74,5 +81,52 @@ func postUserToken(c *gin.Context) {
 	}
 
 	log.Printf("user token received: %v\n", userToken)
+
+	// Store the access token in the context
+	c = context.WithValue(c, accessToken, userToken.Token.AccessToken).(*gin.Context)
+
 	c.IndentedJSON(http.StatusOK, userToken)
+}
+
+func getAccountInformation(c *gin.Context) {
+
+	req, err := http.NewRequest(http.MethodPost, YodleeApiHostUrl+YodleeVerifiedAccountsPath, nil)
+	if err != nil {
+		log.Printf("error creating new request: %v\n", err)
+		return
+	}
+
+	q := req.URL.Query()
+	q.Add("providerAccountId", c.Query("providerAccountId"))
+	req.URL.RawQuery = q.Encode()
+
+	yodlessAccessToken := c.Value(accessToken).(string)
+	req.Header.Set("Api-Version", YodleeAuthApiVersion)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("loginName", "Bearer"+yodlessAccessToken)
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("error sending http request: %v\n", err)
+		return
+	}
+
+	responseBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("error reading http response: %v\n", err)
+		return
+	}
+	res.Body.Close()
+
+	var accountInformation interface{}
+
+	err = json.Unmarshal(responseBytes, &accountInformation)
+	if err != nil {
+		log.Printf("error unmarshalling response bytes: %v\n", err)
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, accountInformation)
 }
