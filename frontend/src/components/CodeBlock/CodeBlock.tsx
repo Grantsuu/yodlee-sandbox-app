f2608270df7aa8141aca3fc0cc9168fd464fdb94
Grantsuu <tnarg122@gmail.com>
2023-10-16 23:26:35 +0000

    Copied tooltip

--- frontend/src/components/CodeBlock/CodeBlock.tsx
+++ frontend/src/components/CodeBlock/CodeBlock.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'; 
 
 export const CodeBlock = () => {
 
@@ -12,13 +13,21 @@ export const CodeBlock = () => {
         navigator.clipboard.writeText("Copied text???");
     }
 
+    const tooltip = (
+        <Tooltip id="tooltip">
+            Copied!
+        </Tooltip>
+      );
+
     return (
         <div className="card text-bg-dark">
             <div className="card-header m-0" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 Header
-                <button type="button" className="btn btn-outline-light" onClick={handleCopyButton}>
-                    {copyIcon}
-                </button>
+                <OverlayTrigger placement="top" trigger="focus" overlay={tooltip}>
+                    <button type="button" className="btn btn-outline-light" onClick={handleCopyButton}>
+                        {copyIcon}
+                    </button>
+                </OverlayTrigger>
             </div>
             <div className="card-body py-2">
                 <SyntaxHighlighter language="javascript" style={stackoverflowDark} className="m-0 py-3">
